f34d340355d263cc2de46a768196baf3b0f49baa
SoySoy4444 <sshoinkyuraa.08.04@hotmail.co.jp>
2026-04-17 23:12:34 +0900

    add country field to address model

--- app/models/address.py
+++ app/models/address.py
@@ -4,7 +4,7 @@ from sqlmodel import Field, SQLModel
 class AddressBase(SQLModel):
     street_nr: str
     city: str
-
+    country: str = "UK"
 
 class Address(AddressBase, table=True):
     id: int = Field(default=None, primary_key=True)
